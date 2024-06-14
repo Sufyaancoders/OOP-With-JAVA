376f7ce21345051a0d21e950c0991410dc7c3567
Rehaan Ahmad <rehaansufyaan1@gmail.com>
2024-06-14 21:40:00 +0530

    class function

---
+++ class2.java
@@ -0,0 +1,28 @@
+public class class2 {
+    //class is name of group of  propertey
+//you creat on data type on class
+/**
+ * class
+ */
+    public static void main (String[] args){
+       student[] students=new student[5];// new data type student
+       student sufyaan =new student();//object
+       sufyaan.name="sufaan ahmad";
+       sufyaan.roll=222;
+   sufyaan.maks=23.3f;
+   System.out.println(sufyaan.roll);
+   System.out.println(sufyaan.maks);
+   System.out.print(sufyaan.name);
+   
+    }
+   }
+   class student2{
+       int roll;
+       float maks;
+       String name;
+       student2(){
+       this.maks=3333.4f;
+       this.name="mohan";
+       this.roll=233;}
+   
+   }
